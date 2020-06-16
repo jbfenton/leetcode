1280f87ddbd74ff0e60ab6b4d1276c6eebc07bf8
jbfenton <josh@jbfenton.dev>
2020-06-16 19:02:39 +1000

    Modified problem 0027 to use constant space

--- python/problems/0027-remove_element/__init__.py
+++ python/problems/0027-remove_element/__init__.py
@@ -3,16 +3,19 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        del_list = []
-
-        for index, item in enumerate(nums):
-            if item == val:
-                del_list.append(index)
+        start = 0
+        end = len(nums) - 1
 
         counter = 0
 
-        for item in del_list:
-            del nums[item - counter]
-            counter += 1
+        while start < len(nums) and start <= end:
+            if nums[end] == val:
+                end -= 1
+            else:
+                if nums[start] == val:
+                    nums[start], nums[end] = nums[end], nums[start]
 
-        return len(nums)
+                start += 1
+                counter += 1
+
+        return counter
